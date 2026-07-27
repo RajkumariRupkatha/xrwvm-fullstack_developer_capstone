67d4ea2e4a9b6007a6dcd21baa7493fd49904989
Rajkumari Rupkatha <rupkatha0472@gmail.com>
2026-07-27 15:33:34 -0400

    Add car data endpoint and admin configuration

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,16 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
 
-app_name = 'djangoapp'
+from . import views
+
+
+app_name = "djangoapp"
+
 urlpatterns = [
-    # # path for registration
+    # Path for login
+    path(route="login", view=views.login_user, name="login"),
 
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    # Path for getting all car makes and models
+    path(route="get_cars", view=views.get_cars, name="getcars"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
