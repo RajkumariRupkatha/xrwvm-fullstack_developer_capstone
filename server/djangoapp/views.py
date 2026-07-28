a308cdba29014c69687561ea42b724a5c2d29848
RajkumariRupkatha <rupkatha0472@gmail.com>
2026-07-28 17:53:38 -0400

    Add CI lint workflow and dealer review routes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -103,6 +103,7 @@ def get_dealer_reviews(request, dealer_id):
     )
 
 
+@csrf_exempt
 def add_review(request):
     if request.user.is_anonymous is False:
         data = json.loads(request.body)
